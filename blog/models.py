199f5b3d6a78efa2dc8719e85a8858cc583b80b8
TMD <kk412027247@gmail.com>
2020-06-17 00:36:46 +0800

    finish add tagging function

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -20,6 +21,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,default='draft')
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -33,6 +35,8 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
---
+++ readme.md
@@ -0,0 +1,15 @@
+
+```shell script
+          
+python3 manage.py makemigrations blog
+
+python3 manage.py migrate
+                       
+python3 manage.py runserver
+
+python manage.py shell
+
+```
+
+
+
