2bf1f7c54835e3002b8845533853adb715c864c6
TMD <kk412027247@gmail.com>
2020-06-18 23:47:57 +0800

    retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from .models import Post, Comment
 from .form import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -12,7 +13,7 @@ def post_list(request, tag_slug=None):
     tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
-    object_list = object_list.filter(tags__in=[tag])
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -39,8 +40,13 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html', {
-        'post': post, 'comments':comments, 'new_comment':new_comment, 'comment_form':comment_form
+        'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form,
+        'similar_posts': similar_posts
     })
 
 
@@ -66,4 +72,4 @@ def post_share(request, post_id):
             send = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'send':send})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'send': send})
