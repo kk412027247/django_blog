08be69da70512b662add46bcc4806197bc221fc7
TMD <kk412027247@gmail.com>
2020-06-20 17:35:09 +0800

    search

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post, Comment
-from .form import EmailPostForm, CommentForm
+from .models import Post
+from .form import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 def post_list(request, tag_slug=None):
@@ -73,3 +74,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'send': send})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form, 'query': query, 'results': results})
--- readme.md
+++ readme.md
@@ -7,9 +7,35 @@ python3 manage.py migrate
                        
 python3 manage.py runserver
 
-python manage.py shell
+python3 manage.py shell
+
+```
+
+#### create a user for your PostgreSQL database
+
+```shell script  
+sudo bash
+su postgres 
+createdb -E utf8 -U blog -h 127.0.0.1 blog   
+```
+
+```python 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER':'blog',
+        'PASSWORD':'123456',
+        'HOST':'127.0.0.1'
+    }
+}
 
 ```
 
 
+# create a superuser
 
+```shell script 
+python3 manage.py createsuperuser
+  
+```
